40ff443ab62671bed84a8547cb3f7d9eedfc776e
Nafitha <nafithajas@gmail.com>
2025-01-08 15:49:38 +0400

    Initial commit

---
+++ src/property/property.dto.ts
@@ -0,0 +1,45 @@
+export class CreatePropertyDto {
+  title: string;
+  description: string;
+  price: number;
+  currency: string;
+  propertyType: string;
+  address: string;
+  city: string;
+  image?:string;
+  state: string;
+  country: string;
+  postalCode: string;
+  coordinates: string;
+  floor?: number;
+  parkingSpaces?: number;
+  balcony?: boolean;
+  garden?: boolean;
+  furnished?: boolean;
+   bedrooms?: number;
+  bathrooms?: number;
+  isFeatured?: boolean;
+}
+
+export class UpdatePropertyDto {
+  title?: string;
+  description?: string;
+  price?: number;
+  currency?: string;
+  propertyType?: string;
+  address?: string;
+  city?: string;
+  image?: string;
+  state?: string;
+  country?: string;
+  postalCode?: string;
+  coordinates?: string;
+  floor?: number;
+  parkingSpaces?: number;
+  balcony?: boolean;
+  garden?: boolean;
+  furnished?: boolean;
+  bedrooms?: number;
+  bathrooms?: number;
+  isFeatured?: boolean;
+}
---
+++ src/property/property.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { PropertyService } from './property.service';
+import { PropertyController } from './property.controller';
+import { PropertySchema } from './property.schema';
+import { CloudinaryService } from 'src/cloudinary/cloudinary.service';
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Property', schema: PropertySchema }])
+  ],
+  providers: [PropertyService,CloudinaryService],
+  controllers: [PropertyController],
+})
+export class PropertyModule {}
