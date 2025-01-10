64288ba6fa7d81026c0d8c5f5867b92df0c76915
Nafitha <nafithajas@gmail.com>
2025-01-10 13:06:11 +0400

    Correct Heroku remote and redeploy

--- my-property-portal/app/apiUrl.js
+++ my-property-portal/app/apiUrl.js
@@ -1,3 +1,3 @@
-//export const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL
+export const API_URL = process.env.NEXT_PUBLIC_BACKEND_API_URL
 
-export const API_URL = 'http://localhost:5000'
+//export const API_URL = 'http://localhost:5000'
