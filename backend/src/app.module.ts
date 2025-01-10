1a68c3b80f376cb178a61c26ae6258b86172306a
Nafitha <nafithajas@gmail.com>
2025-01-10 18:55:48 +0400

    changed project configuration

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -10,8 +10,7 @@ import { join } from 'path';
 @Module({
   imports: [
     ConfigModule.forRoot({
-      envFilePath: join(__dirname, '../../.env'), // Correctly reference the .env file in the project root
-      isGlobal: true,
+      isGlobal: true,  // Makes the config available globally
     }),
     MongooseModule.forRoot(process.env.MONGO_URL ?? (() => {
       throw new Error('MONGO_URL is not defined in the environment variables!');
@@ -25,8 +24,5 @@ import { join } from 'path';
 
 
 export class AppModule {
-  constructor() {
-    console.log('All environment variables:', process.env); // Log environment variables for debugging
-    console.log('MONGO_URL:', process.env.MONGO_URL); // Log MONGO_URL for debugging
-  }
+  constructor() {}
 }
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,17 +4,16 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import path from 'path';
 import * as express from 'express';
 import * as dotenv from 'dotenv';
-import { join } from 'path';
+
 
 async function bootstrap() {
-  // Load environment variables from the .env file in the project root
-  dotenv.config({ path: join(__dirname, '../../.env') });
+  
 
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   // Serve static assets from the 'public/uploads' directory
-  const uploadFolderPath = path.join(process.cwd(), 'public', 'uploads');
-  app.use('/uploads', express.static(uploadFolderPath));
+  // const uploadFolderPath = path.join(process.cwd(), 'public', 'uploads');
+  // app.use('/uploads', express.static(uploadFolderPath));
 
   app.setGlobalPrefix('api');
 
