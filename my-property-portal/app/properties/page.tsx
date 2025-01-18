be7e94eb014dc287fe575f969c91c5ebabb58701
Nafitha <nafithajas@gmail.com>
2025-01-18 23:30:47 +0400

    added auth

--- my-property-portal/app/properties/page.tsx
+++ my-property-portal/app/properties/page.tsx
@@ -46,20 +46,17 @@ const fetchData = async (
 
 // This is a server component that gets searchParams as part of the context
 const PropertiesPage = async ({ searchParams }: { searchParams: Record<string, string> }) => {
-  // Await the searchParams to resolve its values
-  const resolvedSearchParams = await searchParams;
-
-  const page = resolvedSearchParams.page || "1"; // Default page to 1 if undefined
-  const limit = resolvedSearchParams.limit || "6"; // Default limit to 9 if undefined
+  const page = searchParams.page || "1"; // Default page to 1 if undefined
+  const limit = searchParams.limit || "6"; // Default limit to 6 if undefined
 
   // Build filters object based on searchParams
   const filters: Filters = {
-    propertyType: resolvedSearchParams.propertyType,
-    latitude: resolvedSearchParams.latitude,
-    longitude: resolvedSearchParams.longitude,
-    bedrooms: resolvedSearchParams.bedrooms,
-    minPrice: resolvedSearchParams.minPrice,
-    maxPrice: resolvedSearchParams.maxPrice,
+    propertyType: searchParams.propertyType,
+    latitude: searchParams.latitude,
+    longitude: searchParams.longitude,
+    bedrooms: searchParams.bedrooms,
+    minPrice: searchParams.minPrice,
+    maxPrice: searchParams.maxPrice,
   };
 
   // Filter out undefined values from filters
@@ -74,13 +71,11 @@ const PropertiesPage = async ({ searchParams }: { searchParams: Record<string, s
 
   // Fetch data from API (server-side)
   const data = await fetchData(pageNumber, limitNumber, filteredParams);
- 
-
 
   return (
     <div className="max-w-7xl mx-auto my-12">
       {data.properties.length > 0 ? (
-        <PropertyList {...data}  filters={filteredParams}  />
+        <PropertyList {...data}  filters={filteredParams} />
       ) : (
         <div>No properties found.</div>
       )}
